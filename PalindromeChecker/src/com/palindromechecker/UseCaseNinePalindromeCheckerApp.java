4e47b320504a4b21c0d5e936a2b772253188df5e
Utkarsh-37 <up.om.mgs@gmail.com>
2026-03-05 11:57:32 +0530

    Coded UC9 to check Palindrom using Recursion

---
+++ PalindromeChecker/src/com/palindromechecker/UseCaseNinePalindromeCheckerApp.java
@@ -0,0 +1,51 @@
+/*
+ * ================================================================
+ * MAIN CLASS – UseCase9RecursivePalindrome
+ * ================================================================
+ *
+ * Use Case 9: Recursive Palindrome Checker
+ *
+ * Description:
+ * This class validates a palindrome using recursion.
+ *
+ * Characters are compared from the outer positions
+ * moving inward using recursive calls.
+ *
+ * The recursion stops when:
+ * - All characters are matched, or
+ * - A mismatch is found.
+ *
+ * This use case demonstrates divide-and-conquer
+ * logic using method recursion.
+ *
+ * @author Developer
+ * @version 9.0
+ */
+
+package com.palindromechecker;
+import java.util.*;
+
+public class UseCaseNinePalindromeCheckerApp {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input: ");
+		String input = sc.nextLine();
+
+		boolean isPalindrome = check(input, 0, input.length()-1);
+		System.out.println("Is Palindrome? : " + isPalindrome);
+
+	}
+
+	private static boolean check(String s, int start, int end)
+	{
+		if (start >= end) 
+			return true;
+		
+		if (s.charAt(start) != s.charAt(end)) 
+			return false;
+		
+		return check(s, start + 1, end - 1);
+	}
+
+}
